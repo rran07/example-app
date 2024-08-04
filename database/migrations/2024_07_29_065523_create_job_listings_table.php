a038d323b474d5083facae8f858a2758906926b9
Raniel Ronidel <r.ronidel0617@gmail.com>
2024-08-04 17:18:44 +0800

    30 days learning laravel - episode 12

--- database/migrations/2024_07_29_065523_create_job_listings_table.php
+++ database/migrations/2024_07_29_065523_create_job_listings_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('job_listings', function (Blueprint $table) {
             $table->id();
-//            $table->unsignedBigInteger('user_id');
+//            $table->unsignedBigInteger('employer_id');
             $table->foreignIdFor(\App\Models\Employer::class);
             $table->string('title');
             $table->string('salary');
